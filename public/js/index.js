e2b6be7b535a5b23ff4bb0bba82769ef85e45cac
andrew hsieh <andrewhsieh1208@gmail.com>
2019-01-06 13:27:28 -0500

    project setup with hot reloading, webpack/babel, and basic setup

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+import "../styles/styles.css"
+
+// const gameBoard = $("#breakoutBoard");
+// console.log(gameBoard)
+console.log("hi")
+console.log("hellooo")
+// const context = gameBoard.getContext('2d');
+// context.fillStyle('red')
+// context.fillRect(10, 10, 100, 100);
+
+
+
+if(typeof(module.hot) !== 'undefined') {
+  module.hot.accept() // eslint-disable-line no-undef  
+}
